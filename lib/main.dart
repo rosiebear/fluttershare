3e15ad917e7bcb9a998edb03f6ec65f69489c1d8
Emma Rose <emjrose@gmail.com>
2019-11-02 17:11:15 +0000

    Google auth flow and navigation tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttershare/pages/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,9 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterShare',
+      title: 'FlutterShare', 
       debugShowCheckedModeBanner: false,
-      home: Text("Hello World"),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        accentColor: Colors.teal
+      ),
+      home: Home(),
     );
   }
 }
