bef63993cc1607250c17213bdfa1ab8e5b0ef7ad
Emma Rose <emjrose@gmail.com>
2019-11-07 21:27:09 +0000

    CRUD on users

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/widgets/header.dart';
 import 'package:fluttershare/widgets/progress.dart';
+
+final usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -9,10 +12,59 @@ class Timeline extends StatefulWidget {
 
 class _TimelineState extends State<Timeline> {
   @override
+  void initState() {
+    super.initState();
+    //createUser();
+    //updateUser();
+    //deleteUser();
+  }
+
+  createUser() async {
+    await usersRef.document("adjfksdls").setData({
+      "username": "Jeff",
+      "postsCount": 0,
+      "isAdmin": false
+    });
+  }
+
+  updateUser() async{
+    final doc = await usersRef
+      .document("hYMXQFgI9kr3JqZWnE57").get();
+    if (doc.exists) {
+      doc.reference.updateData({"username": "Jenny", "postsCount": 0,
+      "isAdmin": false});
+    }
+  }
+
+  deleteUser() async{
+    final DocumentSnapshot doc = await usersRef
+      .document("hYMXQFgI9kr3JqZWnE57").get();
+
+    if (doc.exists) {
+      doc.reference.delete();
+    }
+  }
+
+  @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context, isAppTitle: true),
-      body: linearProgress(),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: usersRef.snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return circularProgress();
+          }
+          final List<Text> children = snapshot.data.documents
+            .map((doc) => Text(doc['username']))
+            .toList();
+          return Container(
+            child: ListView(
+              children: children,
+            )
+          );
+        },
+      ),
     ); 
   }
 }
